86a89dc72622c7494034ff24bcae095ae0696b8b
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-09-27 19:35:35 +0530

    Navbar added / WS initiated

---
+++ website-redesign/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="manas-logo"></div>
+      <div className="navbar-links">
+        <div className="navbar-link">About</div>
+        <div className="navbar-link">Team</div>
+        <div className="navbar-link">Sponsors</div>
+        <div className="navbar-link">Projects</div>
+        <div className="navbar-link">Contact us</div>
+      </div>
+    </div>
+  );
+}
