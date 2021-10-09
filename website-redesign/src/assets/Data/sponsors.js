2d850df782f41ebb7af42049b7a85fa3454c3385
Jyothsna Ashok <jyothsna1809@gmail.com>
2021-10-10 01:31:19 +0530

    Sponsors

---
+++ website-redesign/src/assets/Data/sponsors.js
@@ -0,0 +1,79 @@
+import bossard from "../Sponsors/bossard.png";
+import ct from "../Sponsors/composites-tomorrow.png";
+import continental from "../Sponsors/continental.png";
+import coolermaster from "../Sponsors/cooler-master.png";
+import gigabyte from "../Sponsors/gigabyte.png";
+import indianbank from "../Sponsors/indian-bank.png";
+import mahindra from "../Sponsors/mahindra-rise.png";
+import microsoft from "../Sponsors/microsoft.png";
+import nationali from "../Sponsors/national-instruments.png";
+import nvidia from "../Sponsors/nvidia.png";
+import ouster from "../Sponsors/ouster.png";
+import pepperlfuchs from "../Sponsors/pepperl-fuchs-text.png";
+import sica from "../Sponsors/sica.png";
+import sick from "../Sponsors/sick.png";
+import simscale from "../Sponsors/simscale.png";
+import tmotor from "../Sponsors/t-motors.png";
+import vector from "../Sponsors/vector.png";
+import veekay from "../Sponsors/veekay-impex.png";
+import ximea from "../Sponsors/ximea.png";
+
+export default [
+  {
+    image: continental
+  },
+  {
+    image: mahindra
+  },
+  {
+    image: sick
+  },
+  {
+    image: nvidia
+  },
+  {
+    image: nationali
+  },
+  {
+    image: ximea
+  },
+  {
+    image: microsoft
+  },
+  {
+    image: coolermaster
+  },
+  {
+    image: vector
+  },
+  {
+    image: gigabyte
+  },
+  {
+    image: pepperlfuchs
+  },
+  {
+    image: indianbank
+  },
+  {
+    image: sica
+  },
+  {
+    image: bossard
+  },
+  {
+    image: veekay
+  },
+  {
+    image: simscale
+  },
+  {
+    image: ouster
+  },
+  {
+    image: tmotor
+  },
+  {
+    image: ct
+  }
+];
--- website-redesign/src/components/Sponsors/Sponsors.jsx
+++ website-redesign/src/components/Sponsors/Sponsors.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import sponsors from "../../assets/Data/sponsors";
 
 export default function Sponsors() {
   return (
     <div className="sponsors">
       <h2>Sponsors</h2>
 
-      <div className="grid-container"></div>
+      <div className="grid-container">
+        {sponsors.map((item, idx) => (
+          <div className="grid-item">
+            <img
+              className="sponsor-logo"
+              src={item.image}
+              key={idx}
+              alt={`project-${idx + 1}`}
+            />
+          </div>
+        ))}
+        <div className="grid-item">
+          <p>Sponsor Us {">"}</p>
+        </div>
+      </div>
     </div>
   );
 }
