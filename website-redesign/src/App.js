bcb7ce31f172748a5a8cf5ad5fe1ba522dee1f2a
Diya Dhabade <diya.dhabade@learner.manipal.edu>
2021-10-09 16:08:24 +0530

    Error page

--- website-redesign/src/App.js
+++ website-redesign/src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import Errorpage from './components/Errorpage/Errorpage';
+import { Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Footer />
-    </div>
+      <div className="App">
+        <Navbar />
+        <Footer />
+        <Router>
+        <Switch>
+         <Route exact path={"/404"}  >
+           <Errorpage />
+           </Route>
+        </Switch>
+        </Router>
+        
+      </div>
   );
 }
 
---
+++ website-redesign/src/components/Errorpage/Errorpage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Errorpage() {
+  return (
+    <div>
+    <div className="nav-container">
+      <div className="manas-logo"></div>
+      <div className="nav-links">
+        <div className="nav-link">About</div>
+        <div className="nav-link">Team</div>
+        <div className="nav-link">Sponsors</div>
+        <div className="nav-link">Projects</div>
+        <div className="nav-link">Contact us</div>
+      </div>
+    </div>
+    <div className="error">
+        <h1>ERROR 404</h1>
+        <h1>ERROR 404</h1>
+        <h1>ERROR 404</h1>
+        <h1>ERROR 404</h1>
+        <h1>ERROR 404</h1>
+       <button className="Button">
+          Go Back
+       </button>
+    </div>
+    </div>
+  );
+}
