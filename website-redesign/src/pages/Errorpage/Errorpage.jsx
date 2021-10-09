54b336682f1eba2c6dcc13d03a3a3f6146e3ee5d
hackerbone <sitaraman.raghav.india@gmail.com>
2021-10-09 19:45:58 +0530

    Error page

--- website-redesign/src/pages/Errorpage/Errorpage.jsx
+++ website-redesign/src/pages/Errorpage/Errorpage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Errorpage() {
   return (
-    <div>
+    <div className="errorpage-container">
       <div className="error">
         <h1>ERROR 404</h1>
         <h1>ERROR 404</h1>
