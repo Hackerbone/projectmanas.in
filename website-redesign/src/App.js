4308afdf56fd7573df8184b12db7bcabd40648ea
hackerbone <sitaraman.raghav.india@gmail.com>
2021-10-09 17:09:08 +0530

    Route done

--- website-redesign/src/App.js
+++ website-redesign/src/App.js
@@ -1,23 +1,22 @@
-import './App.scss';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
-import Errorpage from './components/Errorpage/Errorpage';
-import { Router, Switch, Route } from 'react-router-dom';
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Errorpage from "./components/Errorpage/Errorpage";
+import { Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-      <div className="App">
-        <Navbar />
-        <Footer />
-        <Router>
-        <Switch>
-         <Route exact path={"/404"}  >
-           <Errorpage />
-           </Route>
-        </Switch>
-        </Router>
-        
-      </div>
+    <div className="App">
+      <Navbar />
+
+      <Switch>
+        <Route exact path={"/*"}>
+          <Errorpage />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </div>
   );
 }
 
