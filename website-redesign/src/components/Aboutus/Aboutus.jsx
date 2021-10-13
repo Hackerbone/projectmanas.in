59490e272ac11cd3ecb3b27952d2e9847e43b8c9
Diya Dhabade <diya.dhabade@learner.manipal.edu>
2021-10-13 15:12:06 +0530

    Aboutuspage

---
+++ website-redesign/src/components/Aboutus/Aboutus.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <section>
+          <div class="img1"></div>
+        </section>
+        <section>
+          <div class="img2"></div>
+        </section>
+        <section>
+          <div class="img3"></div>
+        </section>
+      </div>
+      {/* <div className="text1-container">
+        <ul>
+          <h1>About</h1>
+          <h1>Us</h1>
+          <p>
+            Project MANAS is the official AI and Robotics team of MAHE, Manipal
+            established in 2014.
+          </p>
+          <p>
+            We are the Grand Prize winners and won the Lescoe Cup at the
+            Intelligent Ground Vehicle Competition which is held at Michigan.
+            Our aim is to build India’s first driverless car and are among the
+            top 13 participants in the Mahindra’s Rise Prize challenge.
+          </p>
+        </ul>
+        <div className="text2-container">
+          <ul>
+            <h1>Vission & Mission</h1>
+            <p>
+              Project MANAS aims to inspire advanced research in Artificial
+              Intelligence and oversee its implementation enabling automated
+              systems to be made available to generous populous.{" "}
+            </p>
+            <p>
+              Our mission is to uphold the tradition of pushing the limits of
+              Artificial Intelligence and Bringing Bits to Life.
+            </p>
+          </ul>
+        </div>
+  </div> */}
+    </div>
+  );
+}
+export default App;
--- website-redesign/src/pages/Homepage/Homepage.jsx
+++ website-redesign/src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Landing from '../../components/Landing/Landing';
+import Aboutus from '../../components/Aboutus/Aboutus';
 import Sponsors from '../../components/Sponsors/Sponsors';
 import Projects from '../Projects/Projects';
 export default function Homepage() {
   return (
     <>
       <Landing />
+      <Aboutus />
       <Sponsors />
       <Projects />
+
     </>
   );
 }
