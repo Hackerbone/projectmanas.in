d471c44f149a149fc1e951a38a89fac0396de6ac
Jyothsna Ashok <jyothsna1809@gmail.com>
2021-10-17 01:08:12 +0530

    About Us

--- website-redesign/src/components/Aboutus/Aboutus.jsx
+++ website-redesign/src/components/Aboutus/Aboutus.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-function App() {
+import img1 from "../../assets/AboutUs/image1.jpg";
+import Parallax from "react-rellax";
+
+export default function Aboutus() {
   return (
-    <div className="App">
+    <div className="Aboutus">
       <div class="container">
-        <section>
-          <div class="img1"></div>
-        </section>
-        <section>
-          <div class="img2"></div>
-        </section>
-        <section>
-          <div class="img3"></div>
-        </section>
+        <div class="img1"></div>
+
+        <div class="img2"></div>
+
+        <div class="img3"></div>
       </div>
-      {/* <div className="text1-container">
-        <ul>
-          <h1>About</h1>
-          <h1>Us</h1>
+
+      <div className="about-us-text">
+        <div className="text1-container">
+          <h1>
+            About <span>Us</span>
+          </h1>
           <p>
             Project MANAS is the official AI and Robotics team of MAHE, Manipal
             established in 2014.
@@ -27,23 +28,22 @@ function App() {
             Our aim is to build India’s first driverless car and are among the
             top 13 participants in the Mahindra’s Rise Prize challenge.
           </p>
-        </ul>
-        <div className="text2-container">
-          <ul>
-            <h1>Vission & Mission</h1>
-            <p>
-              Project MANAS aims to inspire advanced research in Artificial
-              Intelligence and oversee its implementation enabling automated
-              systems to be made available to generous populous.{" "}
-            </p>
-            <p>
-              Our mission is to uphold the tradition of pushing the limits of
-              Artificial Intelligence and Bringing Bits to Life.
-            </p>
-          </ul>
         </div>
-  </div> */}
+        <div className="text2-container">
+          <h1>
+            <span>Vision</span> & Mission
+          </h1>
+          <p>
+            Project MANAS aims to inspire advanced research in Artificial
+            Intelligence and oversee its implementation enabling automated
+            systems to be made available to generous populous.{" "}
+          </p>
+          <p>
+            Our mission is to uphold the tradition of pushing the limits of
+            Artificial Intelligence and Bringing Bits to Life.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
-export default App;
