eac2121c7aaccb1f9ffdbd43d1f1a5c4df97d343
hackerbone <sitaraman.raghav.india@gmail.com>
2021-10-17 12:22:50 +0530

    Projects section done :tada:

--- website-redesign/src/pages/Projects/Projects.jsx
+++ website-redesign/src/pages/Projects/Projects.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Parallax } from "react-scroll-parallax";
 // import SingleProject from "../../components/Projects/SingleProject";
 import projects from "../../assets/Data/projects";
 export default function Projects() {
-  console.log(projects);
+  const [selected, setSelected] = useState(-1);
   return (
     <div className="projects-section" id="projects">
       <div className="project-header">
@@ -13,14 +13,30 @@ export default function Projects() {
       <div className="projects-container">
         <div className="left-section">
           {projects.map((item, idx) => (
-            <Parallax key={idx} y={[30, -30]} tagOuter="figure">
-              <img src={item.image} alt="projects" className="project-img" />
+            <Parallax key={idx} y={[50, -30]} tagOuter="figure">
+              <img
+                src={item.image}
+                alt="projects"
+                className="project-img"
+                onClick={() => setSelected(idx)}
+              />
+              <div className="project-name">{item.header}</div>
+              {selected === idx ? (
+                <p
+                  className="project-description"
+                  onClick={() => setSelected(!selected)}
+                >
+                  {item.description}
+                </p>
+              ) : (
+                <></>
+              )}
             </Parallax>
           ))}
         </div>
         <div className="right-section">
           {projects.map((item, idx) => (
-            <Parallax strength={200} key={idx} y={[1, 40]} tagOuter="figure">
+            <Parallax strength={200} key={idx} y={[0, 40]} tagOuter="figure">
               <div className="project-content right-content" key={idx}>
                 <h1>{item.header}</h1>
                 <p>{item.description}</p>
